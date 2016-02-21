b089e6d4b1f04dceeace9133ebb6af4a46822618
Henry Fellows <fello056@morris.umn.edu>
2016-02-21 16:05:51 -0600

    Made progress on PRNG

--- src/shuffle/random.clj
+++ src/shuffle/random.clj
@@ -21,47 +21,50 @@
   ([] (inital-numbers seed))
   ([n] (lazy-seq (cons n (inital-numbers
   ;;All constants are Magic, and should never be changed. Or discussed.
-    (lower-32-bits (+ (* 1812433253 (bit-xor n (bit-shift-right n 30))) 1)))))))
+         (lower-32-bits (inc (* 1812433253 (bit-xor n (bit-shift-right n 30))))))))))
+
+(defn- twist
+"Refreshes the vector of seed values."
+  [val]
+  (loop [i 0 new []]
+    (if (>= i 624)
+      new
+      (let
+        [num (nth val i)
+        ;--BEGIN MAGIC--
+         y (lower-32-bits (+
+          (bit-and (nth val i) 0x80000000)
+          (bit-and (nth val (mod (inc i) 624)) 0x7fffffff)))
+         r (bit-shift-right (bit-xor (nth val (mod (+ i 397) 624)) y) 1)
+         result (if (odd? y) ;;Originally 'if y % 2 != 0
+                  (bit-xor r 0x9908b0df)
+                  r)]
+      (recur (inc i) (conj new result))))))
+
+(defn- next
+  "Returns a new pseudorandom number that's probably around int size. No promises."
+  [i val]
+    (as-> (nth i (second val)) num
+    ;;--BEGIN MAGIC--
+      (bit-xor num (bit-shift-right num 11))
+      ;Right shift by 11
+      (bit-xor num (bit-and 2636928640 (bit-shift-left num 7)))
+      ;Left shift by 7, bitwise'd by 2636928640
+      (bit-xor num (bit-and 4022730752 (bit-shift-left num 15)))
+      ;Left shift by 15, bitwise'd by 4022730752
+      (bit-xor num (bit-shift-right num 18))
+      ;Right shift by 15
+      (lower-32-bits num)))
+      ;wait, it's all magic. Damn.
+    ;;--END MAGIC--
+
 
 (defn random-numbers
   "A lazy sequence of pseudorandom numbers created from a Mersenne Twister."
-  ([] (random-numbers 0 (take 624 inital-numbers)) ;;624 is Magic. Don't change it.
+  ([] (random-numbers 0 (take 624 (inital-numbers)))) ;;624 is Magic. Don't change it.
   ([i v] (if (>= 624 i)
-      ;;if the index is >= 624, refresh the seed value vectors
-           (random-numbers 0 (twist val))
-           (lazy-seq (cons (next i v) (random-numbers (inc i) v)))))))
+  ;;if the index is >= 624, refresh the seed value vectors
+           (random-numbers 0 (twist v))
+          (lazy-seq (cons (next i v) (random-numbers (inc i) v))))))
 
-(defn next
-  "Returns a new pseudorandom number that's probably around int size. No promises."
-  [i val]
-      (as-> (nth i (second val)) num
-        ;;--BEGIN MAGIC--
-        (bit-xor num (bit-shift-right num 11))
-          ;Right shift by 11
-        (bit-xor num (bit-and 2636928640 (bit-shift-left num 7)))
-          ;Left shift by 7, bitwise'd by 2636928640
-        (bit-xor num (bit-and 4022730752 (bit-shift-left num 15)))
-          ;Left shift by 15, bitwise'd by 4022730752
-        (bit-xor num (bit-shift-right 18))
-          ;Right shift by 15
-        (lower-32-bits num)))
-        ;;wait, it's all magic. Damn.
-        ;;--END MAGIC--
-
-(defn- twist
-  "Refreshes the vector of seed values."
-  [val]
-  (loop [i 0 new []]
-    (if (>= i 623)
-      new
-      (let
-        [num (nth i val)
-        ;--BEGIN MAGIC--
-        y (lower-32-bits (+
-            (bit-and (nth i val) 0x80000000)]
-            (bit-and (% (nth (inc i) val) 624) 0x7fffffff)))
-        result (if (odd? y) ;;Originally 'if y % 2 != 0'
-          (bit-and (bit-shift-right (bit-xor (nth (% (+ i 397) 624) val) y) 1)
-            0x7fffffff)))
-          ((bit-shift-right (bit-xor (nth (% (+ i 397) 624) val) y) 1))]
-      (recur (inc i) (conj new result)))))
+;;(take 15 (random-numbers))
